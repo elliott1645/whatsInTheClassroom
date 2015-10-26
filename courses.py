c4968d712e47ba5f5731a44bdda0ca7e0c902880
Elliott <elliottweintraub@gmail.com>
2015-10-25 20:27:19 -0400

    got rid of the unnecessary lines

--- courses.py
+++ courses.py
@@ -1,32 +1,20 @@
-%load class.py
+f = open('class.py','r')
 s = open("schedule.pl.mhtml", 'r').read()
 import re
 get_course_info = lambda a: [b for b in a.split('\r\n') if 'Course Info' in b]
 x = s.split("<hr>")
 y = map(get_course_info, x)
-print y
-%load times.py
+#print y
+g = open('times.py','r')
 s = open("schedule.pl.mhtml", 'r').read()
 import re
 get_meeting_info = lambda a: [b for b in a.split('\r\n') if 'Meeting Info' in b]
 r = s.split("<hr>")
 n = map(get_meeting_info, r)
-print n
+#print n
 zip(n,y)
 t = zip(n,y)
 t
-{x:y for (x,y) in t}
-{x0:y0 for (x,y) in t}
-{x[0]:y[0] for (x,y) in t}
-{x[1]:y[1] for (x,y) in t}
-len x
-len x
-len(x)
-len(y)
-{x[0]:y[0] for (x,y) in t}
-t
-t[0
-  ]
 t_new = filter(lambda x : len(x[0]) > 0 and len(x[1])>0, t)
 t_new
 t_new[0]
@@ -35,13 +23,8 @@ t_dict = {x[0]:y[0] for (x,y) in t_new}
 {x[0]:y[0] for (x,y) in t_new}
 t_new
 import re
-help(re.find)
-help(re.search)
-re.search('', t_new[0][0])
-re.search('l', t_new[0][0])
-t_new[0][0]
-t_new[0][0][0]
-%save courses.py
-%save 'courses.py'
-%save courses.py
-%hist
+#re.search('', t_new[0][0])
+#re.search('l', t_new[0][0])
+#t_new[0][0]
+#t_new[0][0][0]
+print t
